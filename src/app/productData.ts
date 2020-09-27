80072698356bc4a7f363bd3dba56621ad62a5a9e
nicoletadinu83 <nicoletadinu1009@gmail.com>
2020-09-27 13:51:03 +0300

    ..

---
+++ src/app/productData.ts
@@ -0,0 +1,18 @@
+export class ProductData{
+
+id : number;
+title : string;
+thumbnail : string;
+price : number;
+
+
+
+
+    
+    // private CategoryDto category;
+
+    
+    // private ProductType productType;
+
+    // private AuthorDto author;
+}
