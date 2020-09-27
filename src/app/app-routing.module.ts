98dc2ae555285bc2100513b6dccda1faa08fcf0f
AlexLungoci <alexandru.lungoci@yahoo.com>
2020-09-27 13:52:54 +0300

    no message

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { OrderComponent } from './order/order.component';
 import { ViewProductListComponent } from './view-product-list/view-product-list.component';
 
 
 const routes: Routes = [
   {path:'view-productList', component:ViewProductListComponent}
+  {path:'order', component:OrderComponent}
 ];
 
 @NgModule({
